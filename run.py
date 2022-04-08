384d424c8a978051bc0475068cb65a9935cba093
JPRodrigues-gh <jp.rodrigues.ba@gmail.com>
2022-04-08 11:48:15 +0000

    Add netflix-rotten-tomatoes-metacritic-imdb.csv file to dataset folder. Import pandas library and verify access to csv file.

--- run.py
+++ run.py
@@ -2,6 +2,7 @@
 # You can delete these comments, but do not change the name of this file
 
 # imports the entire gspread library which provides access to any function, class or method within it
+# install gspread package -- pip3 install gspread google-auth
 import gspread
 # imports the Credentials class which is part of the service_account function from the Google auth library. 
 from google.oauth2.service_account import Credentials
@@ -21,3 +22,11 @@ SHEET = GSPREAD_CLIENT.open('Netflix Rotten Tomatoes Analysis')
 # The above I have taken from the Love Sandwiches project walk through
 
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+# Install pandas package --  pip3 install pandas
+# Import pandas library 
+import pandas as pandas
+dataSet = pandas.read_csv('dataset/netflix-rotten-tomatoes-metacritic-imdb.csv')
+df = dataSet.copy()
+df.head()
+df.info()
